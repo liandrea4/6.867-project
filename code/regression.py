4f293d42689d484ed62b6843a923205c7ae9f535
Andrea Li <liandrea@mit.edu>
2016-12-11 21:20:12 -0500

    Add some results for linear classifiers with basic features on 06-07

--- code/regression.py
+++ code/regression.py
@@ -16,7 +16,28 @@ def get_accuracy(x, y, classifier):
   return float(num_correct) / len(x)
 
 
-def get_data(filename, num_skip, num_training, num_validate, num_testing):
+def get_average_error(x, y, classifier):
+  error_sum = 0.
+
+  for x_i, y_i in zip(x, y):
+    predicted_spread = classifier.predict(x_i)
+
+    error = abs(predicted_spread - y_i)
+    error_sum += error
+
+  if len(x) == 0:
+    return "No data samples"
+
+  return error_sum / len(x)
+
+
+def get_win_lose(away_score, home_score):
+  return home_score > away_score
+
+def get_spread(away_score, home_score):
+  return away_score - home_score
+
+def get_data(filename, num_skip, num_training, num_validate, num_testing, y_fn):
   index = 0
   x_training, y_training, x_validate, y_validate, x_testing, y_testing = [], [], [], [], [], []
 
@@ -29,9 +50,9 @@ def get_data(filename, num_skip, num_training, num_validate, num_testing):
         continue
 
       x = [ float(value) for value in row[5:] ]
-      away_score = row[3]
-      home_score = row[4]
-      y = home_score > away_score
+      away_score = float(row[3])
+      home_score = float(row[4])
+      y = y_fn(away_score, home_score)
 
       if index > num_skip and index <= num_skip + num_training:
         x_training.append(x)
@@ -50,7 +71,7 @@ def get_data(filename, num_skip, num_training, num_validate, num_testing):
   return [x_training, y_training, x_validate, y_validate, x_testing, y_testing]
 
 
-def train_validate_test_classifier(data, classifier):
+def train_validate_test_classifier(data, classifier, metric_fn):
   x_training, y_training = data[0], data[1]
   x_validate, y_validate = data[2], data[3]
   x_testing, y_testing = data[4], data[5]
@@ -58,10 +79,12 @@ def train_validate_test_classifier(data, classifier):
   classifier.fit(x_training, y_training)
   print "coeffs: ", classifier.coef_
 
-  validation_accuracy = get_accuracy(x_validate, y_validate, classifier)
-  print "validation_accuracy: ", validation_accuracy
-  # testing_accuracy = get_accuracy(x_testing, y_testing, classifier)
-  # print "testing_accuracy: ", testing_accuracy
+  training = metric_fn(x_training, y_training, classifier)
+  print "training: ", training
+  # validation = metric_fn(x_validate, y_validate, classifier)
+  # print "validation: ", validation
+  # testing = metric_fn(x_testing, y_testing, classifier)
+  # print "testing: ", testing
 
 
 filename = "data/game_log_06_07.csv"
@@ -74,17 +97,36 @@ num_testing = 300
 
 # Classification y in {0,1} indicates whether or not the home team won (1 = home team won)
 
-
-data = get_data(filename, num_skip, num_training, num_validate, num_testing)
+data_win_lose = get_data(filename, num_skip, num_training, num_validate, num_testing, get_win_lose)
+data_spreads = get_data(filename, num_skip, num_training, num_validate, num_testing, get_spread)
 
 linear_classifier = linear_model.LinearRegression()
 logistic_classifier = linear_model.LogisticRegression()
 lasso_classifier = linear_model.Lasso()
 
-print "Linear classifier"
-train_validate_test_classifier(data, linear_classifier)
+# print "Linear classifier"
+# train_validate_test_classifier(data_spreads, linear_classifier, get_accuracy)
 # print "Logistic classifier"
-# train_validate_test_classifier(data, logistic_classifier)
+# train_validate_test_classifier(data_win_lose, logistic_classifier, get_accuracy)
 # print "Lasso classifier"
-# train_validate_test_classifier(data, lasso_classifier)
+# train_validate_test_classifier(data_win_lose, lasso_classifier, get_accuracy)
 
+
+# print "Linear classifier"
+# train_validate_test_classifier(data_spreads, linear_classifier, get_average_error)
+
+
+# Linear classifier with all basic features on 06-07 (win/lose)
+# training: 0.643
+# validation: 0.653
+# testing: 0.633
+
+# Logistic classifier with all basic features on 06-07 (win/lose)
+# training: 0.643
+# validation: 0.62
+# testing: 0.633
+
+# Linear classifier with all basic features on 06-07 (spreads)
+# training: 9.31
+# validation: 10.03
+# testing: 10.07
